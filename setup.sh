f7503cb32565da02c3ff8633ef5c14cb3d541f73
Alvaro Cortes <58772226+cortesCasado@users.noreply.github.com>
2025-03-02 11:49:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-curl -sL "https://raw.githubusercontent.com/cortesCasado/wsl-dotfile/main/.bashrc" >> ~/.bashrc
-sudo apt install unzip fzf zoxide
 
-# Download and install fnm:
-curl -o- https://fnm.vercel.app/install | bash
-source ~/.bashrc
+# Variables
+PARTIAL_BASH_URL="https://raw.githubusercontent.com/cortesCasado/wsl-dotfile/main/.bashrc"
+CADENA="# From my dotfiles"
+
+# Verifica si la cadena está en el archivo destino
+if ! grep -q "$CADENA" ~/.bashrc; then
+    # Descarga el contenido y lo agrega al final del archivo destino
+    curl -sL "$URL" >> ~/.bashrc
+    echo "Contenido agregado exitosamente."
+else
+    echo "La cadena '$CADENA' ya está presente en el archivo. No se realizó la concatenación del bashrc."
+fi
+
+PROGRAMS="unzip fzf zoxide"
+sudo apt install "$PROGRAMS"
+
+# Verifica si 'fnm' está instalado
+if ! command -v fnm >/dev/null 2>&1; then
+    echo "fnm no está instalado. Ejecutando script de instalación..."
+    curl -fsSL https://fnm.vercel.app/install | bash
+    source ~/.bashrc
+else
+    echo "fnm ya está instalado."
+fi
+
 fnm install --lts
 corepack enable pnpm
 pnpm -v
